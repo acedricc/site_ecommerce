41f2c4c3ba68e7f4b181f08598b701396b27d9b4
acedricc <cedric.akakpo@lepoles.org>
2023-02-03 16:54:09 +0100

    03-02

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -38,6 +38,7 @@ class ProduitType extends AbstractType
                 "choice_label"  => "size",
                 "placeholder"   => "",
                 'multiple' => true,
+                'expanded' => true
             ])
 
             ->add('description')
